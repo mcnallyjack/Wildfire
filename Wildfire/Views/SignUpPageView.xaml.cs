3f08187da1e1d35fc36c1d9bf737671ee876debf
jackmc1337 <jackjjmcnally@gmail.com>
2021-02-07 16:23:50 +0000

    Beginning of Work on Accounts

---
+++ Wildfire/Views/SignUpPageView.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Wildfire.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SignUpPageView : ContentPage
+    {
+        public SignUpPageView()
+        {
+            InitializeComponent();
+        }
+
+        private void SignUp_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
