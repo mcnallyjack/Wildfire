db870bc5f7d3b362d169d175357a5a4d7601f3ae
jackmc1337 <jackjjmcnally@gmail.com>
2021-04-21 14:28:26 +0100

    Navigation errors and general code clean up

--- Wildfire/Views/MapView.xaml.cs
+++ Wildfire/Views/MapView.xaml.cs
@@ -128,24 +128,24 @@ namespace Wildfire.Views
 
         async Task LoadFires()
         {
-            
-
-            var displayFires = await firebaseHelper.GetAllFires();
-            foreach(var i in displayFires)
+            try
             {
-                if (i.Description.Length <= 10)
+                var displayFires = await firebaseHelper.GetAllFires();
+                foreach (var i in displayFires)
                 {
-                    Pin newFire = new Pin()
+                    if (i.Description.Length <= 10)
                     {
-                        Icon = (Device.RuntimePlatform == Device.Android) ? BitmapDescriptorFactory.FromBundle("FlamePins.png") : BitmapDescriptorFactory.FromView(new Image() { Source = "FlamePins.png", WidthRequest = 20, HeightRequest = 20 }),
-                        Label = i.Description.ToString(),
-                        Position = new Position(Convert.ToDouble(i.Latitude), Convert.ToDouble(i.Longitude)),
-                        Address = i.PlaceName.ToString(),
-                        Tag = i.FireID.ToString()
+                        Pin newFire = new Pin()
+                        {
+                            Icon = (Device.RuntimePlatform == Device.Android) ? BitmapDescriptorFactory.FromBundle("FlamePins.png") : BitmapDescriptorFactory.FromView(new Image() { Source = "FlamePins.png", WidthRequest = 20, HeightRequest = 20 }),
+                            Label = i.Description.ToString(),
+                            Position = new Position(Convert.ToDouble(i.Latitude), Convert.ToDouble(i.Longitude)),
+                            Address = i.PlaceName.ToString(),
+                            Tag = i.FireID.ToString()
 
-                    };
-                    map.PinClicked += (sender, e) =>
-                    {
+                        };
+                        map.PinClicked += (sender, e) =>
+                        {
 
                         //await Task.Delay(2000);
                         //await Navigation.PushModalAsync(new ResolveFireInfoView(e.Pin.Label));
@@ -154,29 +154,34 @@ namespace Wildfire.Views
 
 
                     };
-                    map.Pins.Add(newFire);
+                        map.Pins.Add(newFire);
+                    }
+                    else if (i.Description.Length > 10)
+                    {
+                        string newW = String.Concat(i.Description.Select((c, j) => j > 0 && (j % 20) == 0 ? c.ToString() + Environment.NewLine : c.ToString()));
+
+
+                        Pin newFire = new Pin()
+                        {
+                            Icon = (Device.RuntimePlatform == Device.Android) ? BitmapDescriptorFactory.FromBundle("FlamePins.png") : BitmapDescriptorFactory.FromView(new Image() { Source = "FlamePins.png", WidthRequest = 20, HeightRequest = 20 }),
+                            Label = newW.ToString(),
+                            Position = new Position(Convert.ToDouble(i.Latitude), Convert.ToDouble(i.Longitude)),
+                            Address = i.PlaceName.ToString(),
+                            Tag = i.FireID.ToString()
+
+                        };
+                        map.PinClicked += (sender, e) =>
+                        {
+
+                        };
+                        map.Pins.Add(newFire);
+                    }
                 }
-                else if (i.Description.Length > 10)
-                {
-                    string newW = String.Concat(i.Description.Select((c, j) => j > 0 && (j % 20) == 0 ? c.ToString() + Environment.NewLine : c.ToString()));
-
-
-                    Pin newFire = new Pin()
-                    {
-                        Icon = (Device.RuntimePlatform == Device.Android) ? BitmapDescriptorFactory.FromBundle("FlamePins.png") : BitmapDescriptorFactory.FromView(new Image() { Source = "FlamePins.png", WidthRequest = 20, HeightRequest = 20 }),
-                        Label = newW.ToString(),
-                        Position = new Position(Convert.ToDouble(i.Latitude), Convert.ToDouble(i.Longitude)),
-                        Address = i.PlaceName.ToString(),
-                        Tag = i.FireID.ToString()
-
-                    };
-                    map.PinClicked += (sender, e) =>
-                    {
-                       
-                    };
-                    map.Pins.Add(newFire);
-                }    
-            }  
+            }
+            catch(Exception ex)
+            {
+                ex.Message.ToString();
+            }
         }
 
         async Task LoadCurrentPosition()
@@ -543,167 +548,219 @@ namespace Wildfire.Views
 
         private void Search_Clicked(object sender, EventArgs e)
         {
-            popupSearch.IsVisible = true;
+            try
+            {
+                popupSearch.IsVisible = true;
+            }
+            catch(Exception ex)
+            {
+                ex.Message.ToString();
+            }
         }
 
         void RemovePopupTapped(object sender, EventArgs e)
         {
-            popupSearch.IsVisible = false;
+            try
+            {
+                popupSearch.IsVisible = false;
+            }
+            catch(Exception ex)
+            {
+                ex.Message.ToString();
+            }
+            
         }
 
         private async void map_MapClicked(object sender, MapClickedEventArgs e)
         {
-            var permissions = await Permissions.CheckStatusAsync<Permissions.Phone>();
-
-            if (permissions != PermissionStatus.Granted)
+            try
             {
-                permissions = await Permissions.RequestAsync<Permissions.Phone>();
+                var permissions = await Permissions.CheckStatusAsync<Permissions.Phone>();
+
+                if (permissions != PermissionStatus.Granted)
+                {
+                    permissions = await Permissions.RequestAsync<Permissions.Phone>();
+                }
+
+                if (permissions != PermissionStatus.Granted)
+                {
+                    return;
+                }
+                var location = await Geolocation.GetLocationAsync();
+                var plLat = location.Latitude;
+                var plLong = location.Longitude;
+                var placemark1 = await Geocoding.GetPlacemarksAsync(plLat, plLong);
+                var placemarkDetails1 = placemark1?.FirstOrDefault();
+                string locality1 = placemarkDetails1.AdminArea;
+                string areaCode1 = placemarkDetails1.CountryCode;
+                string Place1 = locality1 + ", " + areaCode1;
+
+
+                Pin newFire = new Pin()
+                {
+                    Icon = (Device.RuntimePlatform == Device.Android) ? BitmapDescriptorFactory.FromBundle("FlamePins.png") : BitmapDescriptorFactory.FromView(new Image() { Source = "FlamePins.png", WidthRequest = 20, HeightRequest = 20 }),
+                    Label = "New Fire",
+                    Position = new Position(e.Point.Latitude, e.Point.Longitude),
+                    Address = Place1,
+                    IsDraggable = true
+
+
+                };
+
+                map.Pins.Add(newFire);
+
+                await Task.Delay(500);
+                recent = newFire;
+                var Lat = e.Point.Latitude;
+                var Long = e.Point.Longitude;
+                var Place = Place1;
+                Lat.ToString();
+                Long.ToString();
+                await Navigation.PushModalAsync(new ReportFireInfoView(Lat, Long, Place) { BindingContext = this.BindingContext }, false);
+                }
+                catch(Exception ex)
+                {
+                    ex.Message.ToString();
+                }
             }
-
-            if (permissions != PermissionStatus.Granted)
-            {
-                return;
-            }
-            var location = await Geolocation.GetLocationAsync();
-            var plLat = location.Latitude;
-            var plLong = location.Longitude;
-            var placemark1 = await Geocoding.GetPlacemarksAsync(plLat, plLong);
-            var placemarkDetails1 = placemark1?.FirstOrDefault();
-            string locality1 = placemarkDetails1.AdminArea;
-            string areaCode1 = placemarkDetails1.CountryCode;
-            string Place1 = locality1 + ", " + areaCode1;
-
-
-            Pin newFire = new Pin()
-            {
-                Icon = (Device.RuntimePlatform == Device.Android) ? BitmapDescriptorFactory.FromBundle("FlamePins.png") : BitmapDescriptorFactory.FromView(new Image() { Source = "FlamePins.png", WidthRequest = 20, HeightRequest = 20 }),
-                Label = "New Fire",
-                Position = new Position(e.Point.Latitude, e.Point.Longitude),
-                Address = Place1,
-                IsDraggable = true
-                
-                
-            };
-            
-            map.Pins.Add(newFire);
-
-            await Task.Delay(500);
-            recent = newFire;
-            var Lat = e.Point.Latitude;
-            var Long = e.Point.Longitude;
-            var Place = Place1;
-            Lat.ToString();
-            Long.ToString();
-            await Navigation.PushModalAsync(new ReportFireInfoView(Lat, Long, Place) { BindingContext = this.BindingContext }, false);
-        }
 
        
 
         private async void Location_Button_Clicked(object sender, EventArgs e)
         {
-            var location = await Geolocation.GetLocationAsync();
-            if (location != null)
+            try
             {
-                Pin newLoc = new Pin()
+                var location = await Geolocation.GetLocationAsync();
+                if (location != null)
                 {
-                    Label = "Current Location",
-                    Position = new Position(location.Latitude, location.Longitude)
-                };
-                map.Pins.Add(newLoc);
-                map.MoveToRegion(MapSpan.FromCenterAndRadius(new Position(location.Latitude, location.Longitude), Distance.FromMeters(2000)));
+                    Pin newLoc = new Pin()
+                    {
+                        Label = "Current Location",
+                        Position = new Position(location.Latitude, location.Longitude)
+                    };
+                    map.Pins.Add(newLoc);
+                    map.MoveToRegion(MapSpan.FromCenterAndRadius(new Position(location.Latitude, location.Longitude), Distance.FromMeters(2000)));
+                }
+            }
+            catch(Exception ex)
+            {
+                ex.Message.ToString();
             }
         }
 
         private async void ReportFire_Clicked(object sender, EventArgs e)
         {
-            var permissions = await Permissions.CheckStatusAsync<Permissions.Phone>();
+            try
+            {
 
-            if(permissions != PermissionStatus.Granted)
-            {
-                permissions = await Permissions.RequestAsync<Permissions.Phone>();
-            }
+                var permissions = await Permissions.CheckStatusAsync<Permissions.Phone>();
 
-            if(permissions != PermissionStatus.Granted)
-            {
-                return;
-            }
-            var location = await Geolocation.GetLocationAsync();
-            var plLat = location.Latitude;
-            var plLong = location.Longitude;
-            var placemark1 = await Geocoding.GetPlacemarksAsync(plLat, plLong);
-            var placemarkDetails1 = placemark1?.FirstOrDefault();
-            string locality1 = placemarkDetails1.AdminArea;
-            string areaCode1 = placemarkDetails1.CountryCode;
-            string Place1 = locality1 + ", " + areaCode1;
-            if(location != null)
-            {
-                Pin newLoc = new Pin()
+                if (permissions != PermissionStatus.Granted)
                 {
-                    Icon = (Device.RuntimePlatform == Device.Android) ? BitmapDescriptorFactory.FromBundle("FlamePins.png") : BitmapDescriptorFactory.FromView(new Image() { Source = "FlamePins.png", WidthRequest = 20, HeightRequest = 20 }),
-                    Label = "New Fire",
-                    Position = new Position(location.Latitude, location.Longitude),
-                    Address = Place1
-                };
-               
-                map.Pins.Add(newLoc);
-                var Lat = location.Latitude;
-                var Long = location.Longitude;
-                var Indicator = 0;
-                var placemarks = await Geocoding.GetPlacemarksAsync(Lat, Long);
-                var placemarkDetails = placemarks?.FirstOrDefault();
-                string areaCode = placemarkDetails.AdminArea;
-                string localityName = placemarkDetails.Locality;
-                string Place = localityName + " " + areaCode;
-                Lat.ToString();
-                Long.ToString();
-               
-                await Navigation.PushModalAsync(new ReportFireInfoView(Lat, Long, Place) { BindingContext = this.BindingContext }, false);
+                    permissions = await Permissions.RequestAsync<Permissions.Phone>();
+                }
+
+                if (permissions != PermissionStatus.Granted)
+                {
+                    return;
+                }
+                var location = await Geolocation.GetLocationAsync();
+                var plLat = location.Latitude;
+                var plLong = location.Longitude;
+                var placemark1 = await Geocoding.GetPlacemarksAsync(plLat, plLong);
+                var placemarkDetails1 = placemark1?.FirstOrDefault();
+                string locality1 = placemarkDetails1.AdminArea;
+                string areaCode1 = placemarkDetails1.CountryCode;
+                string Place1 = locality1 + ", " + areaCode1;
+                if (location != null)
+                {
+                    Pin newLoc = new Pin()
+                    {
+                        Icon = (Device.RuntimePlatform == Device.Android) ? BitmapDescriptorFactory.FromBundle("FlamePins.png") : BitmapDescriptorFactory.FromView(new Image() { Source = "FlamePins.png", WidthRequest = 20, HeightRequest = 20 }),
+                        Label = "New Fire",
+                        Position = new Position(location.Latitude, location.Longitude),
+                        Address = Place1
+                    };
+
+                    map.Pins.Add(newLoc);
+                    var Lat = location.Latitude;
+                    var Long = location.Longitude;
+                    reportedIndicator = 0;
+                    recent = newLoc;
+                    var placemarks = await Geocoding.GetPlacemarksAsync(Lat, Long);
+                    var placemarkDetails = placemarks?.FirstOrDefault();
+                    string areaCode = placemarkDetails.AdminArea;
+                    string localityName = placemarkDetails.Locality;
+                    string Place = localityName + " " + areaCode;
+                    Lat.ToString();
+                    Long.ToString();
+
+                    await Navigation.PushModalAsync(new ReportFireInfoView(Lat, Long, Place) { BindingContext = this.BindingContext }, false);
+                }
+            }
+            catch(Exception ex)
+            {
+                ex.Message.ToString();
             }
         }
 
         private async void map_PinClicked(object sender, PinClickedEventArgs e)
         {
+            try
+            {
 
-            if (LoginPageView.token == null)
-            {
-               
+                if (LoginPageView.token == null)
+                {
+
+                }
+                else
+                {
+                    await Task.Delay(100);
+                    await Navigation.PushModalAsync(new ResolveFireInfoView(e.Pin.Label, e.Pin.Address, e.Pin.Tag.ToString()), false);
+                }
             }
-            else
+            catch(Exception ex)
             {
-                await Task.Delay(10);
-                await Navigation.PushModalAsync(new ResolveFireInfoView(e.Pin.Label, e.Pin.Address, e.Pin.Tag.ToString()),false);
-            } 
+                ex.Message.ToString();
+            }
             
         }
 
         public async void SearchPlace_Clicked(object sender, EventArgs e)
         {
-            var search = originEntry.Text;
-            var searchLocation = await Geocoding.GetLocationsAsync(search);
-            
-            var sourceLocations = searchLocation?.FirstOrDefault();
-            if(sourceLocations != null)
+            try
             {
-            Location sourceCoordinates = new Location(sourceLocations.Latitude, sourceLocations.Longitude);
+                var search = originEntry.Text;
+                var searchLocation = await Geocoding.GetLocationsAsync(search);
 
-            Pin pin = new Pin()
-            {
-                Icon = (Device.RuntimePlatform == Device.Android) ? BitmapDescriptorFactory.FromBundle("SearchPins.png") : BitmapDescriptorFactory.FromView(new Image() { Source = "SearchPins.png", WidthRequest= 20, HeightRequest = 20}),
-                Type = PinType.Place,
-                Label = originEntry.Text,
-                Position = new Position(sourceCoordinates.Latitude, sourceCoordinates.Longitude)
-            };
-            map.Pins.Add(pin);
+                var sourceLocations = searchLocation?.FirstOrDefault();
+                if (sourceLocations != null)
+                {
+                    Location sourceCoordinates = new Location(sourceLocations.Latitude, sourceLocations.Longitude);
 
-            map.MoveToRegion(MapSpan.FromCenterAndRadius(new Position(sourceCoordinates.Latitude, sourceCoordinates.Longitude), Distance.FromMeters(500)));
-            originEntry.Text = string.Empty;
-            search = string.Empty;
-            popupSearch.IsVisible = false;
+                    Pin pin = new Pin()
+                    {
+                        Icon = (Device.RuntimePlatform == Device.Android) ? BitmapDescriptorFactory.FromBundle("SearchPins.png") : BitmapDescriptorFactory.FromView(new Image() { Source = "SearchPins.png", WidthRequest = 20, HeightRequest = 20 }),
+                        Type = PinType.Place,
+                        Label = originEntry.Text,
+                        Position = new Position(sourceCoordinates.Latitude, sourceCoordinates.Longitude)
+                    };
+                    map.Pins.Add(pin);
+
+                    map.MoveToRegion(MapSpan.FromCenterAndRadius(new Position(sourceCoordinates.Latitude, sourceCoordinates.Longitude), Distance.FromMeters(500)));
+                    originEntry.Text = string.Empty;
+                    search = string.Empty;
+                    popupSearch.IsVisible = false;
+                }
+                else
+                {
+                    search = string.Empty;
+                    await DisplayAlert("Cant Find Location", "", "Yes");
+                }
             }
-            else
+            catch(Exception ex)
             {
-                search = string.Empty;
-                await DisplayAlert("Cant Find Location", "", "Yes");
+                ex.Message.ToString();
             }
         }
     }
